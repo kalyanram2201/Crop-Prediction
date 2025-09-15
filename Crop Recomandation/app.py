f1cb4fd19f42beb4820acc362356791eb24b8f66
kalyanram2201 <kalyanramakula13@gmail.com>
2025-09-15 23:37:33 +0530

    updates

--- Crop Recomandation/app.py
+++ Crop Recomandation/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -34,4 +35,5 @@ def index():
     return render_template("index.html", crop=crop_result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
